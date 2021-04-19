f59b2b40a485eba3938937c9df770379356cffc0
Deedee <71313732+Dee-glitch@users.noreply.github.com>
2021-04-19 22:21:26 +0200

    Implemented feedback in server.js

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,47 +2,35 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require("http");
-let fs = require("fs");
+const http = require("http");
+const fs = require("fs");
 
 //create a server
-let server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
 	if (req.url === '/index.js') {
 		fs.readFile('./index.js', (error, data) => {
-			if (error) {
-				console.log(error);
-				res.end();
-			} else {
-				res.writeHead(200, {"Content-Type": "text/javascript"})
+			if (err) throw err
+				res.writeHead(200, {"Content-Type": "application/javascript"})
 				res.end(data);
-			}
 		})
 	} 
 	
 	if (req.url === '/style.css') {
-			fs.readFile('./style.css', (error, data) => {
-				if (error) {
-					console.log(error);
-					res.end();
-				} else {
-					res.writeHead(200, {"Content-Type": "text/css"});
-					res.end(data);
-				}
+		fs.readFile('./style.css', (error, data) => {
+			if (err) throw err
+				res.writeHead(200, {"Content-Type": "text/css"});
+				res.end(data);
 		})
 	} 
 	
 	if (req.url === "/") {
-    fs.readFile("./index.html", (error, data) => {
-			if (error) {
-				console.log(error);
-				res.end();
-			} else {
+    		fs.readFile("./index.html", (error, data) => {
+			if (err) throw err
 				res.writeHead(200, {"Content-Type": "text/html"});
 				res.end(data);
-			}
-    });
-  }
+   		 })
+  	}
 })
 
 server.listen(3000); // The server starts to listen on port 3000
